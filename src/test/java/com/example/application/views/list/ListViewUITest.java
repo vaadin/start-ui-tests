e711a7b2f6f7914e88c71835e3db6f5690711d5c
Marco Collovati <mcollovati@gmail.com>
2022-06-16 16:04:29 +0200

    Upgrade to vaadin-testbench-unit 8.1.0.alpha5

--- src/test/java/com/example/application/views/list/ListViewUITest.java
+++ src/test/java/com/example/application/views/list/ListViewUITest.java
@@ -15,14 +15,11 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonWrap;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.combobox.ComboBoxWrap;
+import com.vaadin.flow.component.button.ButtonTester;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridWrap;
-import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.grid.GridTester;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.textfield.TextFieldWrap;
+import com.vaadin.flow.component.textfield.TextFieldTester;
 import com.vaadin.testbench.unit.SpringUIUnitTest;
 import com.vaadin.testbench.unit.TreeOnFailureExtension;
 
@@ -41,20 +38,21 @@ class ListViewUITest extends SpringUIUnitTest {
     CrmService crmService;
 
     ListView view;
-    GridWrap<Grid<Contact>, Contact> grid_;
-    TextFieldWrap<TextField, String> filter_;
-    ButtonWrap addContact_;
+    GridTester<Grid<Contact>, Contact> grid_;
+    TextFieldTester<TextField, String> filter_;
+    ButtonTester addContact_;
 
     @BeforeEach
     void setupReferences() {
         view = navigate(ListView.class);
-        grid_ = wrap(view.grid);
-        filter_ = wrap(view.filterText);
-        addContact_ = $(Button.class).withCaption("Add contact").single();
+        grid_ = test(view.grid);
+        filter_ = test(view.filterText);
+        addContact_ = test($(Button.class).withCaption("Add contact").single());
     }
 
     @Test
     void enterView_editFormNotVisible() {
+        var x = test($(Grid.class).first());
         assertFalse($(ContactForm.class).exists(),
                 "Contact form should not be visible when entering list view");
     }
@@ -114,11 +112,11 @@ class ListViewUITest extends SpringUIUnitTest {
         assertEquals(selected.getStatus(), view.form.status.getValue(),
                 "status");
 
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "save button should be usable");
-        assertTrue(wrap(view.form.delete).isUsable(),
+        assertTrue(test(view.form.delete).isUsable(),
                 "delete button should be usable");
-        assertTrue(wrap(view.form.close).isUsable(),
+        assertTrue(test(view.form.close).isUsable(),
                 "cancel button should be usable");
     }
 
@@ -135,69 +133,69 @@ class ListViewUITest extends SpringUIUnitTest {
         assertTrue(view.form.company.isEmpty(), "company should be empty");
         assertTrue(view.form.status.isEmpty(), "status should be empty");
 
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "save button should not be usable");
-        assertFalse(wrap(view.form.delete).isUsable(),
+        assertFalse(test(view.form.delete).isUsable(),
                 "delete button should not be usable");
-        assertTrue(wrap(view.form.close).isUsable(),
+        assertTrue(test(view.form.close).isUsable(),
                 "cancel button should be usable");
     }
 
     @Test
     void contactForm_invalidValues_errorsShownAndFormCannotBeSubmitted() {
-        TextFieldWrap<TextField, String> firstname_ = wrap(view.form.firstName);
-        TextFieldWrap<TextField, String> lastName_ = wrap(view.form.lastName);
-        TextFieldWrap<EmailField, String> email_ = wrap(view.form.email);
-        ComboBoxWrap<ComboBox<?>, ?> company_ = wrap(view.form.company);
-        ComboBoxWrap<ComboBox<?>, ?> status_ = wrap(view.form.status);
+        var firstname_ = test(view.form.firstName);
+        var lastName_ = test(view.form.lastName);
+        var email_ = test(view.form.email);
+        var company_ = test(view.form.company);
+        var status_ = test(view.form.status);
 
         grid_.select(2);
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "save button should be usable");
 
         firstname_.setValue("");
         assertTrue(firstname_.getComponent().isInvalid());
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "invalid firstname, save button should not be usable");
         firstname_.setValue("some value");
         assertFalse(firstname_.getComponent().isInvalid());
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "invalid firstname, save button should not be usable");
 
         lastName_.setValue("");
         assertTrue(lastName_.getComponent().isInvalid());
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "invalid lastname, save button should not be usable");
         lastName_.setValue("some value");
         assertFalse(lastName_.getComponent().isInvalid());
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "valid lastname, save button should not be usable");
 
         email_.setValue("xxxxxxx");
         assertTrue(email_.getComponent().isInvalid());
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "invalid email, save button should not be usable");
         email_.setValue("a@b.com");
         assertFalse(email_.getComponent().isInvalid());
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "valid email, save button should not be usable");
 
         company_.selectItem(null);
         assertTrue(company_.getComponent().isInvalid());
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "invalid company, save button should not be usable");
         company_.selectItem(company_.getSuggestions().get(1));
         assertFalse(company_.getComponent().isInvalid());
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "valid company, save button should not be usable");
 
         status_.selectItem(null);
         assertTrue(status_.getComponent().isInvalid());
-        assertFalse(wrap(view.form.save).isUsable(),
+        assertFalse(test(view.form.save).isUsable(),
                 "invalid status, save button should not be usable");
         status_.selectItem(status_.getSuggestions().get(1));
         assertFalse(status_.getComponent().isInvalid());
-        assertTrue(wrap(view.form.save).isUsable(),
+        assertTrue(test(view.form.save).isUsable(),
                 "valid status, save button should not be usable");
     }
 
@@ -208,8 +206,7 @@ class ListViewUITest extends SpringUIUnitTest {
         assertTrue($(ContactForm.class).exists(),
                 "Contact form should be visible when adding a new contact");
 
-        ButtonWrap<Button> close = wrap(view.form.close);
-        close.click();
+        test(view.form.close).click();
         assertFalse($(ContactForm.class).exists(),
                 "Contact form should be close after pressing cancel button");
 
@@ -217,7 +214,7 @@ class ListViewUITest extends SpringUIUnitTest {
         assertTrue($(ContactForm.class).exists(),
                 "Contact form should be visible when contact is selected");
 
-        close.click();
+        test(view.form.close).click();
         assertFalse($(ContactForm.class).exists(),
                 "Contact form should be close after pressing cancel button");
 
@@ -232,7 +229,7 @@ class ListViewUITest extends SpringUIUnitTest {
         assertTrue($(ContactForm.class).exists(),
                 "Contact form should be visible when adding a new contact");
 
-        ButtonWrap<Button> delete_ = wrap(view.form.delete);
+        ButtonTester<Button> delete_ = test(view.form.delete);
         assertTrue(delete_.isUsable(), "Delete button should be usable");
 
         delete_.click();
@@ -260,16 +257,13 @@ class ListViewUITest extends SpringUIUnitTest {
         assertTrue($(ContactForm.class).exists(),
                 "Contact form should be visible when adding a new contact");
 
-        TextFieldWrap<TextField, String> firstname_ = wrap(view.form.firstName);
-        TextFieldWrap<TextField, String> lastName_ = wrap(view.form.lastName);
-        TextFieldWrap<EmailField, String> email_ = wrap(view.form.email);
-        ComboBoxWrap<ComboBox<?>, ?> company_ = wrap(view.form.company);
-        ComboBoxWrap<ComboBox<?>, ?> status_ = wrap(view.form.status);
-        ButtonWrap<Button> save_ = wrap(view.form.save);
+        var company_ = test(view.form.company);
+        var status_ = test(view.form.status);
+        var save_ = test(view.form.save);
 
-        firstname_.setValue("XXFirstnameXX");
-        lastName_.setValue("Lastname");
-        email_.setValue("first@last.org");
+        test(view.form.firstName).setValue("XXFirstnameXX");
+        test(view.form.lastName).setValue("Lastname");
+        test(view.form.email).setValue("first@last.org");
         company_.selectItem(company_.getSuggestions().get(2));
         status_.selectItem(status_.getSuggestions().get(3));
 
